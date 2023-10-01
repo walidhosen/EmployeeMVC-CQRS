672207b5a1b9562a8f60d162bd659a632a512f7c
walidhosen <walidhosen23@gmail.com>
2023-10-01 10:24:29 +0600

    Add redoc

--- Employee.Backend/Program.cs
+++ Employee.Backend/Program.cs
@@ -6,7 +6,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(option=>
+{
+    option.SwaggerDoc("v1",
+        new Microsoft.OpenApi.Models.OpenApiInfo
+        {
+            Title = "Employee",
+            Version = "v1",
+            Description = "This is a Eployee Project to see how doucoment can easy be generate"+"for Asp.net Core.",
+
+            Contact = new Microsoft.OpenApi.Models.OpenApiContact
+            {
+                Name = "Alif",
+                Email = "walidhosen23@gmail.com"
+            }
+        });
+});
+
+
 
 builder.Services.MapCore(builder.Configuration);
 
@@ -16,7 +34,17 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    //add it
+    app.UseSwaggerUI(options =>
+    options.SwaggerEndpoint("/swagger/v1/swagger.json",
+    "Demo Documentation v1"));
+
+    app.UseReDoc(options =>
+    {
+        options.DocumentTitle = "Demo Documentation";
+        options.SpecUrl = "/swagger/v1/swagger.json";
+    });
+
 }
 
 app.UseHttpsRedirection();
